05936b97ecd5802ebb3fa03b2acbb893acc0968c
Edwin <developeredwin@gmail.com>
2019-10-09 13:11:53 -0500

    New test suite for individual benchmark function testing

---
+++ test/impltests.jl
@@ -0,0 +1,16 @@
+@testset "Sphere" begin
+    # Simple constructor calls
+    @test Sphere(zeros(4)) ≈ 0.0 # This is the minimum
+    @test Sphere(ones(4)) ≈ 4.0
+    @test Sphere(0.0) ≈ 0.0
+    @test Sphere(5.0) ≈ 25.0
+    # Using multiple dispatch
+    @test isa(evaluate(Sphere(), 0.0), Number)
+end
+
+@testset "Easom" begin
+    @test Easom(fill(3.0, 2)) ≈ -0.94156415
+    @test_throws AssertionError Easom(zeros(4))
+    @test Easom((π, π)) ≈ -1.0 # Check for minimum
+    @test isa(evaluate(Easom(), (2.0, 4.0)), Number)
+end
