d9772feb409638c92272b3ad9897703b66f5a05b
Edwin <developeredwin@gmail.com>
2019-10-08 20:16:21 -0500

    More tests to check the function evaluate

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,6 +11,8 @@ using Test
                 val = Sphere()
                 val.cost == 0.0
     end
-    # Evaluate the function
+    # Evaluate the function with a scalar
     @test evaluate(Sphere(), 0.0) == 0.0
+    # Evaluate the function with an array
+    @test evaluate(Sphere(), zeros(10)) == 0.0
 end
