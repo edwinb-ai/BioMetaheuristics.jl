63ca78c408dc51fa2979a0f1c0c1391a02b54d50
Edwin <developeredwin@gmail.com>
2019-10-09 12:55:00 -0500

    Breaking test sets into different files for better organization

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,5 @@ using Newtman
 using Test
 
 @testset "Newtman.jl" begin
-    # Simple constructor calls
-    @test Sphere(zeros(4)) ≈ 0.0
-    @test Sphere(ones(4)) ≈ 4.0
-    @test Sphere(0.0) ≈ 0.0
-    @test Sphere(5.0) ≈ 25.0
-    # Using multiple dispatch
-    @test evaluate(Sphere(), 0.0) == 0.0
-    @test Easom(fill(3.0, 2)) ≈ -0.94156415
-    @test_throws AssertionError Easom(zeros(4))
+    include("impltests.jl")
 end
