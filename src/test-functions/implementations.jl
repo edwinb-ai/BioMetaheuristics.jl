6dbeb3d0b90d0581992511dd55e1a3a290f5826b
Edwin <developeredwin@gmail.com>
2019-10-09 11:10:15 -0500

    Added a new function to start working on macros and automated code generation

--- src/test-functions/implementations.jl
+++ src/test-functions/implementations.jl
@@ -16,3 +16,12 @@ function evaluate(f::Symbol, x)
     @eval val = $f($x)
     return val
 end
+
+struct Easom <: Unconstrained end
+
+function _easom(x)
+    term_1 = -cos.(x[1]) * cos.(x{2})
+    term_2 = exp.(-(x[1] - π) .^ 2 - (x[2] - π) .^ 2)
+
+    return term_1 * term_2
+end
