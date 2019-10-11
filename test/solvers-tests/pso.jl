3625efc534ed4eebb2e271f5a62011bb10f1ebd3
Edwin <developeredwin@gmail.com>
2019-10-11 17:26:50 -0500

    :white_check_mark:
    Tests for passing a Function type to the optimization algorithms. Too much boilerplate in the runtests.jl file, though.

--- test/solvers-tests/pso.jl
+++ test/solvers-tests/pso.jl
@@ -1,3 +1,7 @@
+function f_sphere(x)
+    return sum(x .^ 2)
+end
+
 @testset "PSO" begin
     @test let
         assert_results = []
@@ -26,6 +30,22 @@
         end
         # if at least 97/100 pass, the it has converged
         if count(assert_results) >= 97
+            true
+        end
+    end
+
+    @test let
+        assert_results = []
+        for k = 1:100
+            for i = 2:10
+                val = PSO(f_sphere, Population(30, i, -5.0, 5.0), 100;
+                    w=0.5, c1=0.25, c2=1.5)
+                push!(assert_results, ≈(val, zeros(i), atol=0.1))
+            end
+        end
+        # if at least 97% of the time converges, the test passes
+        # 900 * .97 = 873
+        if count(assert_results) >= 873
             true
         end
     end
