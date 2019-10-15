4aa571f80a60179f08dafb33c9367405f2eaabc5
Edwin <developeredwin@gmail.com>
2019-10-15 17:09:54 -0500

    :pencil:
    Some docstrings for the Particle struct and its methods

--- src/population/types.jl
+++ src/population/types.jl
@@ -1,13 +1,12 @@
 """
     Individual
+
+Abstract super-type for types that contain their own information.
 """
 abstract type Individual end
 
 export Particle, Population
 
-"""
-    Particle
-"""
 mutable struct Particle{T<:AbstractArray, V<:AbstractFloat} <: Individual
     x::T
     v::T
@@ -25,12 +24,43 @@ mutable struct Particle{T<:AbstractArray, V<:AbstractFloat} <: Individual
 end
 
 """
-    Particle
+    Particle(x::T, v::T, x_best::T, a::V, b::V) where {T<:AbstractArray, V<:AbstractFloat}
+
+A type that can hold information about current position, current velocity,
+the _best_ candidate to a solution, as well as defining the bounds.
+The dimensions of the `Particle` are inferred from the length of the arrays.
+
+# Arguments
+- `x`: Array that holds the **positions** of possible solutions.
+- `v`: Array that holds **velocities** related to `x`.
+- `x_best`: An element of `x` that determines the best position for the particle.
+- `a`: lower bound for `x`
+- `b`: upper bound for `v`
+
+# Example
+```julia
+p = Particle(zeros(3), rand(3), zeros(3), -1.0, 1.0)
+```
 """
 Particle(x::T, v::T, x_best::T, a::V, b::V) where {T<:AbstractArray, V<:AbstractFloat} =
     Particle{T, V}(x, v, x_best, a, b)
 
-function _particle(a::T, b::T, n::V) where {T<:AbstractFloat, V<:Int}
+"""
+    Particle(a::T, b::T, n::V) where {T<:AbstractFloat, V<:Int}
+
+`Particle` that can be created randomly using the bounds and the dimension needed.
+
+# Arguments
+- `a`: lower bound for `x`
+- `b`: upper bound for `v`
+- `n`: dimension for `x`, `v`, and `x_best`.
+
+# Example
+```julia
+p = Particle(-1.0, 1.0, 3)
+```
+"""
+function Particle(a::T, b::T, n::V) where {T<:AbstractFloat, V<:Int}
     @assert n > 0 "Dimension is always positive"
 
     x = a .+ (rand(T, n) * (b - a))
@@ -40,9 +70,6 @@ function _particle(a::T, b::T, n::V) where {T<:AbstractFloat, V<:Int}
     return Particle(x, v, x_best, a, b)
 end
 
-"""
-    Population
-"""
 mutable struct Population end
 
 """
@@ -54,7 +81,7 @@ function Population(num_particles::T, dim::T, a::V, b::V) where {T<:Int, V<:Abst
 
     container = Vector{Particle}(undef, num_particles)
     for idx in eachindex(container)
-        container[idx] = _particle(a, b, dim)
+        container[idx] = Particle(a, b, dim)
     end
 
     return container
@@ -68,7 +95,7 @@ function Population(dim::T, a::V, b::V) where {T<:Int, V<:AbstractFloat}
 
     container = Vector{Particle}(undef, 5)
     for idx in eachindex(container)
-        container[idx] = _particle(a, b, dim)
+        container[idx] = Particle(a, b, dim)
     end
 
     return container
