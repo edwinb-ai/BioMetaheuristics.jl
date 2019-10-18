9f298b08bcf5bc066c95f3d4c16635d5f8b1ce25
Edwin <developeredwin@gmail.com>
2019-10-18 14:52:07 -0500

    :pencil2:
    Fixed some typos in docstrings

--- test/solvers-tests/pso.jl
+++ test/solvers-tests/pso.jl
@@ -6,15 +6,13 @@ end
 
     @test let
         assert_results = []
-        for k = 1:1
-            for i = 2:30
-                val = PSO(Sphere(), Population(30, i, -10.0, 10.0), 20000)
-                push!(assert_results, ≈(val, zeros(i), atol=1e-13))
-            end
+        for k = 1:50
+            val = PSO(Sphere(), Population(30, 30, -10.0, 10.0), 20000)
+            push!(assert_results, ≈(val, zeros(30), atol=1e-13))
         end
         # if at least 97% of the time converges, the test passes
-        # 150 * .97 = 145
-        if count(assert_results) >= 145
+        # 50 * .97 = 48
+        if count(assert_results) >= 48
             true
         end
     end
@@ -22,29 +20,27 @@ end
     @test let
         assert_results = []
         for k = 1:50
-            val = PSO(Easom(), Population(35, 2, -100.0, 100.0), 5000)
+            val = PSO(Easom(), Population(35, 2, -100.0, 100.0), 10000)
             design = [π, π]
-            result = ≈(val, design, atol=1e-4)
+            result = ≈(val, design, atol=1e-8)
             push!(assert_results, result)
         end
         # if at least 97/100 pass, the it has converged
-        if count(assert_results) >= 45
+        if count(assert_results) >= 48
             true
         end
     end
 
-    # @test let
-    #     assert_results = []
-    #     for k = 1:50
-    #         for i = 2:30
-    #             val = PSO(f_sphere, Population(30, i, -10.0, 10.0), 20000)
-    #             push!(assert_results, ≈(val, zeros(i), atol=0.1))
-    #         end
-    #     end
-    #     # if at least 97% of the time converges, the test passes
-    #     # 150 * .97 = 145
-    #     if count(assert_results) >= 145
-    #         true
-    #     end
-    # end
+    @test let
+        assert_results = []
+        for k = 1:50
+            val = PSO(f_sphere, Population(30, 30, -10.0, 10.0), 20000)
+            push!(assert_results, ≈(val, zeros(30), atol=1e-13))
+        end
+        # if at least 97% of the time converges, the test passes
+        # 50 * .97 = 48
+        if count(assert_results) >= 48
+            true
+        end
+    end
 end
