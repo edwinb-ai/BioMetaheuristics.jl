67f25c48c15b2d3825c10f8d069522875637a3e9
Edwin <developeredwin@gmail.com>
2020-01-09 21:50:47 -0600

    :sparkle:
    Added documentation for the new Ackley function

--- src/test-functions/implementations.jl
+++ src/test-functions/implementations.jl
@@ -14,7 +14,7 @@ where ``d`` is the dimension of the input vector ``\mathbf{x}``.
 struct Sphere <: Unconstrained end
 
 function _sphere(x)
-    return sum(x .^ 2)
+    return sum(x.^2)
 end
 
 @doc raw"""
@@ -36,7 +36,30 @@ function _easom(x)
     @assert length(x) == 2 "This is a 2D function"
 
     term_1 = -cos.(x[1]) * cos.(x[2])
-    term_2 = exp.(-(x[1] - π) .^ 2 - (x[2] - π) .^ 2)
+    term_2 = exp.(-(x[1] - π).^2 - (x[2] - π).^2)
 
     return term_1 * term_2
 end
+
+@doc raw"""
+    Ackely
+
+An unconstrained implementation of the d-dimensional
+Ackely function defined as:
+
+```math
+f(\mathbf{x}) = 
+```
+
+where ``x_1`` and ``x_2`` refer to the first and second element of the
+input vector ``\mathbf{x}``.
+"""
+struct Ackely <: Unconstrained end
+
+# function _ackley(x)
+
+#     term_1 = -cos.(x[1]) * cos.(x[2])
+#     term_2 = exp.(-(x[1] - π).^2 - (x[2] - π).^2)
+
+#     return term_1 * term_2
+# end
