b70aa4d8898cc484e97df30262d326f20d6c60e4
edwinb-ai <developeredwin@gmail.com>
2020-03-19 18:56:50 -0600

    Incremented the number of iterations for the Generalized Simulated Annealing test

--- test/solvers-tests/simulated_annealing.jl
+++ test/solvers-tests/simulated_annealing.jl
@@ -35,7 +35,7 @@ end
         dimension = 2
         ground_truth = 0.0
         val = GeneralSimulatedAnnealing(rosenbrock2d, -5.0, 5.0, dimension;
-        low_temp = 10000, qv = 2.5, qa = 1.0000001)
+        low_temp = 30000, qv = 2.5, qa = 1.0000001)
         @show val
         isapprox(val.min, ground_truth, atol = 1e-2)
     end
