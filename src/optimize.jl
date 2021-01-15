b485eeca8720a5c05098c06a52a9dfe25d456582
edwinb-ai <developeredwin@gmail.com>
2021-01-14 18:10:58 -0600

    :pencil:
    Adding some documentation for the common optimization interface.

--- src/optimize.jl
+++ src/optimize.jl
@@ -1,9 +1,36 @@
+"""
+    function optimize(f, sol, range, method::PopulationBase;
+        rng=nothing,
+        iters=2_000,
+        pop_size=30,
+        kwargs...
+    ) -> OptimizationResults
+
+The `optimize` constitutes an interface to quickly dispatch over all the possible implementations.
+
+In particular, this function dispatches over all `PopulationBase` implementations.
+
+# Arguments
+
+- `f`: The function to optimize.
+- `sol`: A candidate solution, should be an `AbstractArray`. The dimension of this array will determine the dimension of the problem.
+- `range`: A collection that contains just two elements, regarding the global bounds of the search space.
+- `method`: A `PopulationBase`, e.g. `PSO` for Particle Swarm Optimization.
+
+# Keword arguments
+
+- `rng`: An `AbstractRNG` object. This will be used for creating the population of solutions as well as for the implementation itself. If you wish to enforce reproducibility of results, create an RNG object with a set seed. If you do not pass an RNG object, a `Xorshifts.Xoroshiro128Plus` RNG will be create, randomly seeded.
+- `iters`: The total number of iterations for the main loop in the implementation.
+- `pop_size`: The total size of the population. The larger the population, the longer it will take to converge. A good population size is somewhere between 20-35.
+- `kwargs`: Are keyword arguments passed on to the particular implementation. Each implementation has its own keyword arguments so check the documentation for a specific implementation in order to modify these arguments.
+"""
 function optimize(f, sol, range, method::PopulationBase;
     rng=nothing,
     iters=2_000,
     pop_size=35,
     kwargs...
 )
+    @assert length(range) == 2 "No more than two global bounds."
     # If no RNG is passsed, create a randomly seeded one
     if isnothing(rng)
         rng = Xorshifts.Xoroshiro128Plus()
